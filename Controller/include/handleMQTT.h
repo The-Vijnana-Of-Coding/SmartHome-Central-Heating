624cd6ff7a130bc0021a9c88db89f2399346b83b
caton-mark <100426775+The-Vijnana-Of-Coding@users.noreply.github.com>
2023-10-29 15:00:21 +0000

    Updated controller mqtt logic

--- Controller/include/handleMQTT.h
+++ Controller/include/handleMQTT.h
@@ -38,25 +38,57 @@ void checkTemp(float temp)
 
 void handleMQTT(const char* topic, const char* message)
 {
-    // Parse the JSON
   DynamicJsonDocument jsonDoc(256); // Adjust the buffer size as needed
   DeserializationError error = deserializeJson(jsonDoc, message);
 
-  if (error) {
+  if (error)
+  {
     Serial.print("Failed to parse JSON: ");
     Serial.println(error.c_str());
-  } else {
-    // Check if the JSON contains a "temperature" field
-    if (jsonDoc.containsKey("temperature")) {
+  }
+  else
+  {
+    if (strcmp(topic, sub_temp) == 0)
+    {
       float temperature = jsonDoc["temperature"];
       Serial.print("Received temperature: ");
       Serial.println(temperature);
-      
       checkTemp(temperature);
-    } else {
-      Serial.println("JSON message does not contain a 'temperature' field.");
+    }
+    else if (strcmp(topic, sub_override_set) == 0)
+    {
+      if(jsonDoc["chstate"] == "ON")
+      {
+        if(digitalRead(relayPin))
+        {
+            debugln("Relay Already ON!");
+        }
+        else
+        {
+            digitalWrite(relayPin, HIGH);
+            debugln("Switching Relay ON!");
+        }
+      } 
+      else
+      {
+        if(!digitalRead(relayPin))
+        {
+            debugln("Relay Already OFF!");
+        }
+        else
+        {
+            digitalWrite(relayPin, LOW);
+            debugln("Switching Relay OFF!");
+        }
+      }
+    }
+    else
+    {
+      Serial.println("JSON message lacks 'temperature' field.");
     }
   }
 }
+
+
 
 #endif
